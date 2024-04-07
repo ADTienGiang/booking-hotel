b73c54381077104a586b3dcc362f2e4ac4487f40
van <nguyenthevann6@gmail.com>
2024-04-07 19:17:06 +0700

    commit 1

--- backend/config/configAPI.js
+++ backend/config/configAPI.js
@@ -3,6 +3,4 @@
 //     facebook_api_secret   :     "f2269c8a78b3859e75941a7d0fd43bac", //Điền App Secret ở đây
 //     callback_urlfb     :     "http://api.toyshop.id.vn/auth/facebook/callback",
 //     callback_urlgg     :     "http://localhost:3000/auth/google/callback",
-//     GOOGLE_CLIENT_ID :"895502637028-m3skj20084u3uscth6cf8nrlaa3t7q8i.apps.googleusercontent.com",
-//     GOOGLE_CLIENT_SECRET :"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"
 //   } 
